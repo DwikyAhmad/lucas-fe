46dbf80667817177e021fb256e038217a614fba4
rakhadavin <rakha.davin@ui.ac.id>
2024-10-26 16:29:58 +0700

    fix: Summary Order

--- src/app/productsService/[CategorieId]/[productId]/checkout/page.tsx
+++ src/app/productsService/[CategorieId]/[productId]/checkout/page.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 
-const page = () => {
+const summary = () => {
   return (
-      <div>
-          Checkout Summary
-      
+    <div className='bg-primaryBlueNavy h-full w-full'>
+      <div className="summary-title font-poppins font-bold text-2xl">SUMMARY ORDER</div>
+      <div className="content">
+        <div className="left-content items-detail"></div>
+        <div className="right-content courier-address">
+          <div className="courier"></div>
+          <div className="address"></div>
+        </div>
+      </div>
+      <div className="transaction-summary">
+        <div className="title"></div>
+        <div className="summary-content"></div>
+        <div className="caution"></div>
+        <div className="button"></div>
+      </div>
     </div>
   )
 }
 
-export default page
+export default summary
