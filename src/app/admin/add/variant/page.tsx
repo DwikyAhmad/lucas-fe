f45a476a0e1cf67471d23e0a618d844e3f73b634
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2024-11-03 15:59:55 +0700

    feat: fully functional variant form

--- src/app/admin/add/variant/page.tsx
+++ src/app/admin/add/variant/page.tsx
@@ -1,5 +1,23 @@
 import VariantForm from "@/components/variantAdmin/VariantForm";
+import API_URL from "@/utils/utils";
+import axios from "axios";
 
-export default function page() {
-    return <VariantForm />;
+export default async function page() {
+    let categories = [];
+    try {
+        const response = await axios.get(`${API_URL}/category`);
+        categories = response.data.categories;
+    } catch (error) {
+        console.log(error);
+    }
+
+    let products = [];
+    try { 
+        const response = await axios.get(`${API_URL}/product`);
+        products = response.data.products;
+    } catch (error) {
+        console.log(error);
+    }
+
+    return <VariantForm categories={categories} products={products}/>;
 }
--- src/firebase/storage.js
+++ src/firebase/storage.js
@@ -5,7 +5,7 @@ export const uploadFile = async (file, folder) => {
   try {
     const filename = nanoid();
     const fileExtension = file.name.split('.').pop();
-    const filePath = `${folder}${filename}.${fileExtension}`;
+    const filePath = `${folder}/${filename}.${fileExtension}`;
     const fileRef = storage.file(filePath);
     
     const buffer = Buffer.from(await file.arrayBuffer());
@@ -22,6 +22,26 @@ export const uploadFile = async (file, folder) => {
 
     return downloadURL[0];
   } catch (error) {
+    throw error;
+  }
+};
+
+export const extractFilePathFromUrl = (url) => {
+  const match = url.match(/\/o\/(.*?)\?alt=media/);
+  if (match && match[1]) {
+    return decodeURIComponent(match[1]);
+  }
+  throw new Error('Invalid download URL');
+};
+
+export const deleteFile = async (downloadURL) => {
+  try {
+    const filePath = extractFilePathFromUrl(downloadURL);
+    const fileRef = storage.file(filePath);
+    await fileRef.delete();
+    console.log(`File ${filePath} deleted successfully.`);
+  } catch (error) {
+    console.error(`Failed to delete file:`, error);
     throw error;
   }
 };
