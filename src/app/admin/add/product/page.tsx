fb5d14a892c6feb6e4dd87e83b426d3a5cd13796
rakhadavin <rakha.davin@ui.ac.id>
2024-11-04 15:50:57 +0700

    Fix: change return value

--- src/app/admin/add/product/page.tsx
+++ src/app/admin/add/product/page.tsx
@@ -1,16 +1,6 @@
-// import ProductForm from "@/components/productAdmin/ProductForm";
-// import axios from "axios";
-// import API_URL from "@/utils/utils";
 
 
-// export default async function page() {
-//     let categories = [];
-//     try {
-//         const response = await axios.get(`${API_URL}/category`);
-//         categories = response.data.categories;
-//     } catch (error) {
-//         console.log(error);
-//     }
-
-//     return <ProductForm categories={categories} />;
-// }
+export default async function page() {
+    return <div></div>
+//  
+}
