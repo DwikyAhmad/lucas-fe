6d00fc8938fb29980102a52d7b299f27db40e982
rakhadavin <rakha.davin@ui.ac.id>
2024-11-17 22:07:42 +0700

    fix: wrapping overflow product

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -58,14 +58,13 @@ const ProductDetails = async () => {
 
 
   const selectFilter = (value:string) => { 
-    // listFilter.push(e.target.value);
     setListFilter((prev)=>prev.includes(value)?prev:[...listFilter,value])
-
-    console.log("LIST FILTER ATAS : ", listFilter)
-
-
   }
-  
+
+  const formatRupiah = (price:number) => {
+    return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' , minimumFractionDigits: 0, // Tidak menampilkan angka di belakang koma
+      maximumFractionDigits: 0,  }).format(price);
+  };  
   
  
   useEffect(() => {
@@ -73,9 +72,7 @@ const ProductDetails = async () => {
     if (param == undefined || param == null || param.toString() == "" && listFilter.length == 0) {
       setSearch("all")
     } else if (param != undefined || param != null || param != "" ) {
-      
       setSearch(filter)
-      // listFilter.push(filter)
       setListFilter([...listFilter, filter])
     }
     const getProducts = async () => {
@@ -101,54 +98,54 @@ const ProductDetails = async () => {
       <Navbar />
 
       <HeaderProduct pageTitle="ALL PRODUCTS" className='text-primaryBlueNavy flex flex-col   w-full pt-4 items-center ' />
-      <HeaderCategories title={'GENERIC PRODUCT'} />
-      <div className="flex lg:flex-row sm:flex-col flex-col items-centter justify-start align-middle w-full px-6 mt-2 gap-8 border-2">
+      <div className="flex lg:flex-row sm:flex-col flex-col items-centter justify-start align-middle w-full px-6 mt-2 gap-8 border-2 border-black">
 
         <div className="filter text-white bg-redBricks h-full lg:w-max w-full lg:rounded-xl rounded-full px-4 py-2 gap-2 lg:pt-5 lg:px-10 lg:pb-20 border flex lg:flex-col lg:items-start items-center align-middle justify-center "  >
         <h2 className='md:text-2xl text-xl mb-5 font-semibold lg:flex  hidden'>FILTER</h2>
-          
           {categories.map((category,index) => (
             <CardFilter  key= {index} name={category.name} isChecked={false}  value={category.name} onFilterClick={(value)=>selectFilter(value)}/>
           ))}
       </div>
-
-
-        {/* card Product */}
-          {listProduct.map((product,index)=>(
-            <div className="products-container-500 w-full h-full flex gap-4 mb-8  " key={index}>
-          <div className="text-black border md:w-max w-min flex items-center  flex-col border-black border-opacity-20 hover:shadow-xl text-3xl rounded-2xl transition-transform duration-10 ease-in-out  lg:py-1 py-0 pb-2   " style={{ cursor: "pointer;" }} onClick={() => router?.push(`/products/${product.id}`)}>
-            <div className="product-image border    rounded-xl drop-shadow-2xl border-black border-opacity-20 flex items-center justify-center align-middle w-auto h-max mb-2 scale-75 md:scale-100  ">
-              <Image src={'https://i.pinimg.com/564x/ee/62/96/ee62964178d22165482a2c1a0343cb2a.jpg'} className=' rounded-xl w-max  border border-black ' onClick={() => router?.push(`/products/${product.id}`)} alt={''} width={150} height={100}></Image>
-            </div>
-            <div className="metadata md:px-4 px-2 ">
-              <div className="desc md:mx-4  ">
-                    <div className="title font-semibold font-poppins lg:text-2xl text-xl text-primaryBlueNavy">{product.name}</div>
-                    <div className="title text-xs font-medium text-black text-opacity-30">{ product.type}</div>
-              </div>
-              <div className="price flex justify-between  md:mt-8 mt-4  md:gap-4 gap-2">
-                    <p className="border  border-primaryBlack md:rounded-xl px-1 md:py-1 py-1 lg:text-xl text-lg text-nowrap font-semibold hover:bg-primaryBlack hover:text-white transition-transform duration-300 ease-in-out hover:scale-105 drop-shadow-2xl md:w-full h-max w-max items-center align-middle  justify-center content-center flex rounded-full" style={{ cursor: "pointer;" }} onClick={() => router?.push('/products/1')} >Rp { product.price}</p>
-                <div className="marketplace">
-                  <div className="border border-primaryBlack rounded-xl px-2 py-1 text-2xl font-semibold h-full items-center flex bg-primaryYellow text-white transition-transform duration-300 ease-in-out hover:scale-105 md:scale-100 scale-75drop-shadow-2xl hover:bg-darkYellow " style={{ cursor: "pointer;" }}> <IoCart />
+      <div className='w-full flex flex-col items-center align-top justify-start border'>
+              {listFilter.map((filter,index) => (
+                <div key={index} className='w-full h-full flex flex-col'> 
+                <HeaderCategories title={filter} />
+                  <div className='w-full  flex-wrap  flex flex-row items-center align-top justify-start  border-black border  '>
+                    {listProduct
+                      .filter((product) => product?.categoryName && product.categoryName.includes(filter) ) // Filter produk sesuai kategori
+                      .map((product) => (
+                        <div className=" w-min h-max flex flex-col gap-4 mb-8 border-2 m-2 border-red-600 " key={product.id}>
+                          <div className="text-black border md:w-max w-min flex items-center  flex-col border-black border-opacity-20 hover:shadow-xl text-3xl rounded-2xl transition-transform duration-10 ease-in-out  lg:py-1 py-0 pb-2   " style={{ cursor: "pointer;" }} onClick={() => router?.push(`/products/${product.id}`)}>
+                            <div className="product-image border    rounded-xl drop-shadow-2xl border-black border-opacity-20 flex items-center justify-center align-middle w-auto h-max mb-2 scale-75 md:scale-100  ">
+                              <Image src={'https://i.pinimg.com/564x/ee/62/96/ee62964178d22165482a2c1a0343cb2a.jpg'} className=' rounded-xl w-max  border border-black ' onClick={() => router?.push(`/products/${product.id}`)} alt={''} width={150} height={100}></Image>
+                            </div>
+                              <div className="metadata md:px-4 px-2 ">
+                                <div className="desc md:mx-4  ">
+                                      <div className="title font-semibold font-poppins lg:text-2xl text-xl text-primaryBlueNavy">{product.name}</div>
+                                      <div className="title text-xs font-medium text-black text-opacity-30">{ product.type}</div>
+                                </div>
+                                <div className="price flex justify-between  md:mt-8 mt-4  md:gap-4 gap-2">
+                                      <p className="border  border-primaryBlack md:rounded-xl px-1 md:py-1 py-1 lg:text-xl text-lg text-nowrap font-semibold hover:bg-primaryBlack hover:text-white transition-transform duration-300 ease-in-out hover:scale-105 drop-shadow-2xl md:w-full h-max w-full items-center align-middle  justify-center content-center flex rounded-full" style={{ cursor: "pointer;" }} onClick={() => router?.push('/products/1')} >{ formatRupiah(product.price)}</p>
+                                  <div className="marketplace">
+                                    <div className="border border-primaryBlack rounded-xl px-2 py-1 text-2xl font-semibold h-full items-center flex bg-primaryYellow text-white transition-transform duration-300 ease-in-out hover:scale-105 md:scale-100 scale-75drop-shadow-2xl hover:bg-darkYellow " style={{ cursor: "pointer;" }}> <IoCart />
+                                    </div>
+                                  </div>
+                                </div>
+                            </div>
+                          </div>
+                      </div>
+                      ))}
                   </div>
-                </div>
-              </div>
-            </div>
-
-
-          </div>
-
-
-
-        </div>
-
-
-
-            
-          ))
-
-          
-          }
+                  </div>
+              ))}
+       
         
+
+
+
+
+      </div>
+          
 
 
       </div>
